9bb70eb747e8a271b63f118b14e198835ee15b93
Michael Mercier <mickours@gmail.com>
2017-03-22 13:40:46 +0100

    Add vidéo support

--- foldery.sh
+++ foldery.sh
@@ -19,7 +19,7 @@ thumbs="thumbs"
 thumbs_abs_path="$PWD/$thumbs"
 gallery="$folder.html"
 root_path="$PWD"
-pictures=$(ls "$folder" | egrep -i "jpg$|jpeg$" | egrep -v 'thumb')
+pictures=$(ls "$folder" | egrep -i "jpg$|jpeg$|mp4$|avi$" | egrep -v 'thumb')
 
 #
 # Process pictures, create thumbnails if necessary
@@ -40,37 +40,42 @@ do
    then
       pictitle=$(basename "$picture")
 
-      # Uncomment one of the four paragraphs below to achieve different effects.
-      # (Only have one paragraph at a time uncommented).
-      #
+      if [ -z "$(file -i \"$picture\" | grep video)" ]
+      then
+          ffmpeg -i "$picture" -t 2 -r 0.5 "$thumbpic".jpg
+      else
+        # Uncomment one of the four paragraphs below to achieve different effects.
+        # (Only have one paragraph at a time uncommented).
+        #
 
 
-      # Option 1. Simple thumbnails with no effects.
-      #echo convert "$picture" -resize 10% "$thumbpic"
-      #convert "$picture" -resize 10% "$thumbpic"
+        # Option 1. Simple thumbnails with no effects.
+        #echo convert "$picture" -resize 10% "$thumbpic"
+        #convert "$picture" -resize 10% "$thumbpic"
 
 
-      # Option 2. Put a simple frame around each picture, no caption.
-      #echo montage -resize 10% -frame 5 -geometry +0+0 "$picture" "$thumbpic"
-      #montage -resize 10% -frame 5 -geometry +0+0 "$picture" "$thumbpic"
+        # Option 2. Put a simple frame around each picture, no caption.
+        #echo montage -resize 10% -frame 5 -geometry +0+0 "$picture" "$thumbpic"
+        #montage -resize 10% -frame 5 -geometry +0+0 "$picture" "$thumbpic"
 
 
-      # Option 3. Put a simple frame round each picture with a caption at the bottom (-label)
-      #echo montage -resize 10% -pointsize 20 -label "$pictitle" "$picture" -frame 5 -geometry +0+0 "$thumbpic"
-      #montage -resize 10% -pointsize 20 -label "$pictitle" "$picture" -frame 5 -geometry +0+0 "$thumbpic"
+        # Option 3. Put a simple frame round each picture with a caption at the bottom (-label)
+        #echo montage -resize 10% -pointsize 20 -label "$pictitle" "$picture" -frame 5 -geometry +0+0 "$thumbpic"
+        #montage -resize 10% -pointsize 20 -label "$pictitle" "$picture" -frame 5 -geometry +0+0 "$thumbpic"
 
 
-      # Option 4. Put a "polaroid" effect on each picture, including a caption.  Picture is framed,
-      # rotated with shadow.  If $angle is zero there is no rotation.
-      # Note: the "-repage" is there to offet the rotated/"polaroided" within its actual
-      # (unrotated) frame.  Without -repage, there is clipping where the shared/rotated 
-      # image goes beyond the image border.
-      #
-      convert -resize 10% $picture png:small.png
-      angle=$(($RANDOM % 20 - 10))
-      #angle=0
-      convert -set caption "$pictitle" small.png -pointsize 28 -background black -polaroid $angle -repage +10+5 png:polaroid.png
-      convert polaroid.png -background white -flatten $thumbpic
+        # Option 4. Put a "polaroid" effect on each picture, including a caption.  Picture is framed,
+        # rotated with shadow.  If $angle is zero there is no rotation.
+        # Note: the "-repage" is there to offet the rotated/"polaroided" within its actual
+        # (unrotated) frame.  Without -repage, there is clipping where the shared/rotated 
+        # image goes beyond the image border.
+        #
+        convert -resize 10% $picture png:small.png
+        angle=$(($RANDOM % 20 - 10))
+        #angle=0
+        convert -set caption "$pictitle" small.png -pointsize 28 -background black -polaroid $angle -repage +10+5 png:polaroid.png
+        convert polaroid.png -background white -flatten $thumbpic
+      fi
    fi
 done
 
@@ -91,7 +96,10 @@ popd
 ###############################################################################
 #
 # Create index.html file
-#
+
+# get zipfile size
+size=$(ls -lh "$zipfile" | awk '{print $5}')
+
 cat > "$gallery" <<%
 <html>
 <head>
@@ -105,15 +113,17 @@ cat > "$gallery" <<%
 
 for picture in $pictures
 do
-   thumbpic="$thumbs/thumb_$picture"
+    thumbpic="$thumbs/thumb_$picture"
 
-   pictitle=$picture
+    pictitle=$picture
 
-   echo "<a href=\"$folder/$picture\"><img src=$thumbpic alt=$thumbpic title=\"$pictitle\"></a>" >> "$gallery"
+    if [ -z "$(file -i \"$picture\" | grep video)" ]
+    then
+      echo "<a href=\"$folder/$picture\"><img src=$thumbpic.jpg alt=$thumbpic title=\"$pictitle\"></a>" >> "$gallery"
+    else
+      echo "<a href=\"$folder/$picture\"><img src=$thumbpic alt=$thumbpic title=\"$pictitle\"></a>" >> "$gallery"
+    fi
 done
-
-# get zipfile size
-size=$(ls -lh "$zipfile" | awk '{print $5}')
 
 cat >> "$gallery" <<%
 <p>Full album: <a href="$zipfile">"$zipfile"</a> ($size)</p>
